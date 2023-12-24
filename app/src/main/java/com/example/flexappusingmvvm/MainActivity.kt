6366f37c5e0e6abaed2dc9b0ff193514284dbfdb
tucke075 <tucke075@csusm.edu>
2023-12-24 15:54:30 -0800

    Initial commit to github of an android studio project.

---
+++ app/src/main/java/com/example/flexappusingmvvm/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.flexappusingmvvm
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
